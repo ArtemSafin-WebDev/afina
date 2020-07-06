88a71091e2709ebe13d60bcc95cf77da1331ebeb
Artem Safin <artemsafin.webdev@gmail.com>
2020-07-06 04:09:30 +0300

    Текущий прогресс

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 import polyfills from './polyfills';
 import detectTouch from './detectTouch';
 import customSelects from './customSelects';
+import phoneMask from './phoneMask';
 
 document.addEventListener('DOMContentLoaded', function() {
     polyfills();
     detectTouch();
     customSelects();
+    phoneMask();
 });
 
 window.addEventListener('load', function() {
